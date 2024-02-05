c4cc44a073a6b00ce0b7d34c13e02c748a80bf58
TylerSmall19 <intrasolarmusic@gmail.com>
2024-02-05 17:35:26 -0600

    add the new background on the screen and center it

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,46 +1,52 @@
 import { Scene } from 'phaser';
+import { IMAGE_CONSTANTS } from '../config/image_constants';
 
 export class Preloader extends Scene
 {
-    constructor ()
-    {
-        super('Preloader');
-    }
+  constructor ()
+  {
+      super('Preloader');
+  }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+  init ()
+  {
+    const cameraWidth = this.cameras.main.width
+    const cameraHeight = this.cameras.main.height
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    const bg = this.add.image(0, 0, 'background')
+    .setOrigin(0);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    bg.setScale(Math.max(cameraWidth / bg.width, cameraHeight / bg.height))
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress: number) => {
+    //  A simple progress bar. This is the outline of the bar.
+    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
+    //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
+    const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
 
-        });
-    }
+    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+    this.load.on('progress', (progress: number) => {
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
-        this.load.setPath('assets');
+      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
+      bar.width = 4 + (460 * progress);
 
-        this.load.image('logo', 'logo.png');
-    }
+    });
+  }
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
+  preload ()
+  {
+    //  Load the assets for the game - Replace with your own assets
+    this.load.setPath('assets');
 
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
-    }
+    this.load.image(IMAGE_CONSTANTS.MENU_LOGO, 'menu_logo.png');
+  }
+
+  create ()
+  {
+    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+    //  For example, you can define global animations here, so we can use them in other scenes.
+
+    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    this.scene.start('MainMenu');
+  }
 }
