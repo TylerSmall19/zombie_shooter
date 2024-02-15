587aec043d9eff6d459f55198eecccb37b16fc61
TylerSmall19 <intrasolarmusic@gmail.com>
2024-02-14 18:03:02 -0600

    Add a config for the player controls

---
+++ src/config/controls.ts
@@ -0,0 +1,8 @@
+export const controls = {
+  getPlayerControls: () => {
+    return {
+      playerLeft: Phaser.Input.Keyboard.KeyCodes.A,
+      playerRight: Phaser.Input.Keyboard.KeyCodes.D
+    }
+  }
+}
